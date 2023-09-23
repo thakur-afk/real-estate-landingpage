9c58876598b4958a8d7641eb877b3ddef7f119ea
Harshit Thakur <harshitthakur@ternaengg.ac.in>
2023-09-23 16:49:07 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,4 @@
+import "./style.css";
+document.getElementById("side_bar").addEventListener("click", () => {
+  document.getElementById("bar_content").classList.toggle("hidden");
+});
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      fontSize: {
+        sm: "0.75rem",
+        base: ".875rem",
+        md: "1rem",
+        lg: "1.25rem",
+        xl: "1.5rem",
+        "2xl": "1.75rem",
+        "3xl": "2.25rem",
+        "4xl": "3rem",
+      },
+    },
+  },
+  plugins: [],
+};
